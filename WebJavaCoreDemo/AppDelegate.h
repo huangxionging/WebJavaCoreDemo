ec29d9ffe2c520c84c734714759589f29ffa1697
黄雄 <huangxionging@163.com>
2016-11-30 10:16:53 +0800

    Initial Commit

---
+++ WebJavaCoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebJavaCoreDemo
+//
+//  Created by huangxiong on 2016/11/30.
+//  Copyright © 2016年 huangxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebJavaCoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebJavaCoreDemo
+//
+//  Created by huangxiong on 2016/11/30.
+//  Copyright © 2016年 huangxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
